2cc131f94510aa8b722b6edad2b18bcf9121380c
Pietro <bonfus@gmail.com>
2025-12-07 11:01:18 +0100

    removed wring factor

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from charge import charge_efg
 from ase.io.cube import read_cube_data
 
 rho, atoms = read_cube_data('charge.cube')
-# Ry to Ha
-rho *= 2
 
-ions = ions_efg(atoms, gmax=40, rmax=20)
+#atoms.set_scaled_positions(np.array([[0.,0.,0.], [0.5 ,0.5, 0.5]]))
+
+ions = ions_efg(atoms, gmax=60, rmax=20)
 electrons = charge_efg(atoms, rho)
 
 print('ions',ions)
